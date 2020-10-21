41b21173d4bb2708fc75366135efd1132ea5e978
lerislozano <llozanocordero16@correo.unicordoba.edu.co>
2020-10-21 17:56:14 -0500

    apirestful aporte de redes

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express(); 
 const port = 3000;  
 app.get('/', (req, res) => { 
-  res.send('Hi, Mi nombre es leris!'); 
+  res.send('Buenos dias, mi nombre es andres'); 
 }) 
 
 app.listen(port, () => { 
